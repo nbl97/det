cd73b6bb610a78ce023a7f8e2da2e9c1ac851416
nbl97 <1035833260@qq.com>
2021-05-23 04:10:57 +0800

    1

--- pai.sh
+++ pai.sh
@@ -7,4 +7,5 @@ echo $MASTER_PORT
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR \
-  --master_port=$MASTER_PORT tools/train.py --data-path /root/FastBaseline/data/imagenet
+  --master_port=$MASTER_PORT tools/train.py configs/swin/cascade_mask_rcnn_swin_small_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
+  --work-dir /mnt/configblob/users/bolin/s2b_22302_det/

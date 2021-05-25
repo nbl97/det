3e88a41488f6b789d9e40b96a834976ee384c649
nbl97 <1035833260@qq.com>
2021-05-25 14:20:35 +0800

    1

--- pai.sh
+++ pai.sh
@@ -7,6 +7,6 @@ echo $MASTER_PORT
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR \
-  --master_port=$MASTER_PORT tools/train.py configs/swin/cascade_mask_rcnn_swin_small_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
-  --work-dir /mnt/configblob/users/bolin/s2b_22302_det/ \
-  --cfg-options model.pretrained=/mnt/configblob/users/bolin/base_22302.pth
+  --master_port=$MASTER_PORT tools/train.py configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py \
+  --work-dir /mnt/configblob/users/bolin/b14_det_3x_16gpu/ \
+  --cfg-options model.pretrained=/mnt/configblob/users/bolin/b14.pth
